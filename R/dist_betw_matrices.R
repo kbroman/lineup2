69e763727ddf7417e8df038235493b3b45a63900
Karl Broman <kbroman@gmail.com>
2017-12-01 09:51:14 -0600

    Enable multi-core for dist_betw_matrices()

--- R/dist_betw_matrices.R
+++ R/dist_betw_matrices.R
@@ -9,6 +9,10 @@
 #' @param distance Indicates whether to use Euclidean distance
 #'     (`"rmsd"` for root mean square difference) or the mean absolute
 #'     difference (`"mad"`).
+#' @param cores Number of CPU cores to use, for parallel calculations.
+#' (If `0`, use [parallel::detectCores()].)
+#' Alternatively, this can be links to a set of cluster sockets, as
+#' produced by [parallel::makeCluster()].
 #'
 #' @return If `x` is `p` by `n` and `y` is `p` by `m`, then the result
 #'     is a `n` by `m` matrix whose (i,j)th element is the distance
@@ -17,7 +21,7 @@
 #' @seealso [corr_betw_matrices()]
 #' @export
 dist_betw_matrices <-
-    function(x,y, distance=c("rmsd", "mad"))
+    function(x,y, distance=c("rmsd", "mad"), cores=1)
 {
     distance <- match.arg(distance)
 
@@ -28,13 +32,29 @@ dist_betw_matrices <-
         stop("x and y should have the same number of columns")
 
     # the C++ functions need the transpose of x and y
+    x <- t(x)
+    y <- t(y)
+
+    # set up cluster
+    cores <- setup_cluster(cores)
+
     if(distance=="rmsd") {
-        result <- rmsd_betw_matrices(t(x), t(y))
+        func <- rmsd_betw_matrices
     } else {
-        result <- mad_betw_matrices(t(x), t(y))
+        func <- mad_betw_matrices
     }
 
-    dimnames(result) <- list(rownames(x), rownames(y))
+    if(cores==1) {
+        result <- func(x, y)
+    }
+    else { # multi-core
+        func_by_xcol <- function(i) func(x[,i,drop=FALSE], y)
+
+        result <- cluster_lapply(cores, 1:ncol(x), func_by_xcol)
+        result <- matrix(unlist(result), ncol=ncol(y), byrow=TRUE)
+    }
+
+    dimnames(result) <- list(colnames(x), colnames(y))
 
     result
 }
