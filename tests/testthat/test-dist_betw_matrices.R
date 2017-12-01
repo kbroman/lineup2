215d257337885dacba5f40743f39191daffaf39b
Karl Broman <kbroman@gmail.com>
2017-11-30 21:12:24 -0600

    Add more tests of dist_betw_matrices

--- tests/testthat/test-dist_betw_matrices.R
+++ tests/testthat/test-dist_betw_matrices.R
@@ -4,10 +4,20 @@ test_that("dist_betw_matrices() works", {
 
     set.seed(20171130)
     n <- 5
-    x <- matrix(rnorm(n*10), nrow=n)
+    p <- 10
+    x <- matrix(rnorm(n*p), nrow=n)
     expect_equivalent(dist_betw_matrices(x, x), as.matrix(dist(x)))
 
     rownames(x) <- LETTERS[1:n]
     expect_equal(dist_betw_matrices(x, x), as.matrix(dist(x)))
+
+
+    m <- 3
+    y <- matrix(rnorm(m*p), nrow=m)
+    rownames(y) <- letters[1:m]
+
+    expect_error(dist_betw_matrices(x, y[,1:5]))
+
+    expect_equal(dist_betw_matrices(x, y), as.matrix(dist(rbind(x,y)))[1:n, n + 1:m])
 
 })
