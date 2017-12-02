fb7fe3d78fdc5d2542e40a96330d0037526da9ff
Karl Broman <kbroman@gmail.com>
2017-12-02 02:56:43 -0600

    Add align_matrix_rows() and align_matrix_cols()
     - If col/row names present, subset and order the rows of two matrices
       so that they're aligned

---
+++ R/align_matrices.R
@@ -0,0 +1,29 @@
+# align the rows of two matrices
+align_matrix_rows <-
+    function(x, y)
+{
+    rnx <- rownames(x)
+    rny <- rownames(y)
+
+    if(is.null(rnx) || is.null(rny)) return(list(x=x,y=y))
+
+    common <- rnx[rnx %in% rny]
+
+    # return just the rows that are in common
+    list(x=x[common,,drop=FALSE], y=y[common,,drop=FALSE])
+}
+
+# align the columns of two matrices
+align_matrix_cols <-
+    function(x, y)
+{
+    cnx <- colnames(x)
+    cny <- colnames(y)
+
+    if(is.null(cnx) || is.null(cny)) return(list(x=x,y=y))
+
+    common <- cnx[cnx %in% cny]
+
+    # return just the cols that are in common
+    list(x=x[,common,drop=FALSE], y=y[,common,drop=FALSE])
+}
