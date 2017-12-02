5bb25ece802235ea07b8c2662e6bae71fdd74c6c
Karl Broman <kbroman@gmail.com>
2017-12-02 02:58:42 -0600

    In align_matrix_rows/cols, make sure the names are all distinct

--- R/align_matrices.R
+++ R/align_matrices.R
@@ -7,6 +7,10 @@ align_matrix_rows <-
 
     if(is.null(rnx) || is.null(rny)) return(list(x=x,y=y))
 
+    if(length(unique(rnx)) != length(rnx) ||
+       length(unique(rny)) != length(rny))
+        stop("The row names aren't distinct.")
+
     common <- rnx[rnx %in% rny]
 
     # return just the rows that are in common
@@ -21,6 +25,10 @@ align_matrix_cols <-
     cny <- colnames(y)
 
     if(is.null(cnx) || is.null(cny)) return(list(x=x,y=y))
+
+    if(length(unique(cnx)) != length(cnx) ||
+       length(unique(cny)) != length(cny))
+        stop("The column names aren't distinct.")
 
     common <- cnx[cnx %in% cny]
 
