a62590ee76b79300d52512d9df44e4c9ef90332c
Karl Broman <kbroman@gmail.com>
2026-06-06 11:19:05 -0500

    link to vignette in lineup2-package.Rd

--- R/lineup2-package.R
+++ R/lineup2-package.R
@@ -2,5 +2,7 @@
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib lineup2, .registration=TRUE
 #'
+#' @section Vignette:
+#' - [R/lineup2 User Guide](https://kbroman.org/lineup2/lineup2.html)
 #'
 "_PACKAGE"
